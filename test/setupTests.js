f8734d7254728392e1c282e04bfcfa030150922f
Evans Hauser <evanshauser@gmail.com>
2018-03-26 17:49:32 -0700

    remove unused script test and made useragent consistent

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,5 +1,8 @@
 /* @flow */
 
+if(window && window.navigator)
+  Object.defineProperty(window.navigator, 'userAgent', {value: 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Safari/537.2'});
+
 const localStorageMock = {
   getItem: jest.fn(),
   setItem: jest.fn(),
